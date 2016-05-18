6fd739d0609a3b85d7bab8f48d940fb8aac3d725
Mike <mike.toggweiler@tegonal.com>
2016-05-18 13:24:28 +0200

    OO-175: refactoring of ui to manage vertriebe and vertriebsarten

--- app/scripts/abotypen/detail/lieferungen/lieferungen.model.js
+++ app/scripts/abotypen/detail/lieferungen/lieferungen.model.js
@@ -5,7 +5,7 @@
 angular.module('openolitor')
   .factory('LieferungenListModel', function($resource, API_URL) {
     return $resource(API_URL +
-      'abotypen/:abotypId/vertriebsarten/:vertriebId/lieferungen/:id', {
+      'abotypen/:abotypId/vertriebe/:vertriebId/lieferungen/:id', {
         id: '@id',
         abotypId: '@abotypId',
         vertriebId: '@vertriebId'
