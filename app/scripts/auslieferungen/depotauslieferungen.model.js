411f03abc381b8fc653f26d6f8cc7d0f660c856d
Thomas Rawyler <thomas.rawyler@tegonal.com>
2016-06-28 16:49:41 +0200

    OO-249: auslieferung aktion

--- app/scripts/auslieferungen/depotauslieferungen.model.js
+++ app/scripts/auslieferungen/depotauslieferungen.model.js
@@ -4,7 +4,15 @@
  */
 angular.module('openolitor')
   .factory('DepotAuslieferungenModel', function($resource, API_URL) {
-    return $resource(API_URL + 'depotauslieferungen/:id', {
+    return $resource(API_URL + 'depotauslieferungen/:id/:extendedPath/:aktion', {
       id: '@id'
+    }, {
+      ausliefern: {
+        method: 'POST',
+        params: {
+          extendedPath: 'aktionen',
+          aktion: 'ausliefern'
+        }
+      }
     });
   });
