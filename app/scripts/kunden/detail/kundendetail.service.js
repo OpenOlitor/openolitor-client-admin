1854c7f7bd9fb71a6dd204437af242d13e07c576
Thomas Rawyler <thomas.rawyler@tegonal.com>
2016-07-27 17:25:21 +0200

    OO-152: aktivieren/deaktivieren von login

---
+++ app/scripts/kunden/detail/kundendetail.service.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ */
+angular.module('openolitor-admin')
+  .factory('KundenDetailService', ['$http', 'API_URL',
+    function($http, API_URL) {
+      var service = {
+        deletePerson: deletePerson,
+        disableLogin: disableLogin,
+        enableLogin: enableLogin
+      };
+
+      return service;
+
+      function deletePerson(kundeId, personId) {
+        return $http.delete(API_URL + 'kunden/' + kundeId + '/personen/' + personId);
+      };
+
+      function disableLogin(kundeId, personId) {
+        return $http.post(API_URL + 'kunden/' + kundeId + '/personen/' + personId + '/aktionen/logindeaktivieren');
+      };
+
+      function enableLogin(kundeId, personId) {
+        return $http.post(API_URL + 'kunden/' + kundeId + '/personen/' + personId + '/aktionen/loginaktivieren');
+      };
+    }
+  ]);
