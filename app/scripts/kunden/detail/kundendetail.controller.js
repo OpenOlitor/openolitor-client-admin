62f56f65bccdb141b44d6f3408881cbadfa44218
Thomas Rawyler <thomas.rawyler@tegonal.com>
2016-11-01 14:20:18 +0100

    calling save

--- app/scripts/kunden/detail/kundendetail.controller.js
+++ app/scripts/kunden/detail/kundendetail.controller.js
@@ -51,7 +51,7 @@ angular.module('openolitor-admin')
           }
         },
         onExecute: function() {
-          return $scope.kunde.$save();
+          return $scope.save();
         }
       }, {
         label: 'E-Mail an alle Ansprechpersonen',
