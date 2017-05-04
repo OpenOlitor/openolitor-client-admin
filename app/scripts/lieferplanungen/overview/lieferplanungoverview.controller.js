8d44a3867e85837cb8cc19cfeb6bd57d926e2d4c
Thomas Rawyler <thomas.rawyler@tegonal.com>
2017-05-04 11:34:17 +0200

    OO-591: search query for lieferplanungen

--- app/scripts/lieferplanungen/overview/lieferplanungoverview.controller.js
+++ app/scripts/lieferplanungen/overview/lieferplanungoverview.controller.js
@@ -4,15 +4,52 @@
  */
 angular.module('openolitor-admin')
   .controller('LieferplanungOverviewController', ['$q', '$scope', '$filter',
-    'LieferplanungModel', 'NgTableParams', 'msgBus', '$location', 'localeSensitiveComparator',
-    function($q, $scope, $filter, LieferplanungModel, NgTableParams, msgBus,
-      $location, localeSensitiveComparator) {
+    'LieferplanungModel', 'NgTableParams',
+    'FilterQueryUtil', 'OverviewCheckboxUtil',
+    'msgBus', '$location', 'localeSensitiveComparator',
+    function($q, $scope, $filter, LieferplanungModel, NgTableParams,
+      FilterQueryUtil, OverviewCheckboxUtil,
+      msgBus, $location, localeSensitiveComparator) {
 
       $scope.entries = [];
+      $scope.filteredEntries = [];
       $scope.loading = false;
+
+      $scope.search = {
+        query: '',
+        queryQuery: '',
+        filterQuery: ''
+      };
+
+      $scope.checkboxes = {
+        checked: false,
+        checkedAny: false,
+        items: {},
+        css: '',
+        ids: []
+      };
 
       $scope.hasData = function() {
         return $scope.entries !== undefined;
+      };
+
+      // watch for check all checkbox
+      $scope.$watch(function() {
+        return $scope.checkboxes.checked;
+      }, function(value) {
+        OverviewCheckboxUtil.checkboxWatchCallback($scope, value);
+      });
+
+      // watch for data checkboxes
+      $scope.$watch(function() {
+        return $scope.checkboxes.items;
+      }, function() {
+        OverviewCheckboxUtil.dataCheckboxWatchCallback($scope);
+      }, true);
+
+      $scope.toggleShowAll = function() {
+        $scope.showAll = !$scope.showAll;
+        $scope.tableParams.reload();
       };
 
       if (!$scope.tableParams) {
@@ -29,15 +66,24 @@ angular.module('openolitor-admin')
             isExpanded: true
           },
           exportODSModel: LieferplanungModel,
+          exportODSFilter: function() {
+            return {
+              f: $scope.search.filterQuery
+            };
+          },
           getData: function(params) {
             if (!$scope.entries) {
               return;
             }
             // use build-in angular filter
-            var orderedData = $filter('filter')($scope.entries, params.filter());
+            var filteredData = $filter('filter')($scope.entries, $scope
+              .search.queryQuery);
+            var orderedData = $filter('filter')(filteredData, params.filter());
             orderedData = params.sorting ?
               $filter('orderBy')(orderedData, params.orderBy(), true, localeSensitiveComparator) :
               orderedData;
+
+            $scope.filteredEntries = filteredData;
 
             params.total(orderedData.length);
             return orderedData.slice((params.page() - 1) * params.count(),
@@ -51,16 +97,29 @@ angular.module('openolitor-admin')
         if ($scope.loading) {
           return;
         }
-        $scope.tableParams.reload();
 
         $scope.loading = true;
-        $scope.entries = LieferplanungModel.query({}, function() {
+        $scope.entries = LieferplanungModel.query({
+          f: $scope.search.filterQuery
+        }, function() {
           $scope.tableParams.reload();
           $scope.loading = false;
+          $location.search('q', $scope.search.query);
         });
       }
 
-      search();
+      var existingQuery = $location.search().q;
+      if (existingQuery) {
+        $scope.search.query = existingQuery;
+      }
+
+      $scope.$watch('search.query', function() {
+        $scope.search.filterQuery = FilterQueryUtil.transform($scope.search
+          .query);
+        $scope.search.queryQuery = FilterQueryUtil.withoutFilters($scope.search
+          .query);
+        search();
+      }, true);
 
       $scope.createNewLieferplanung = function() {
         $scope.newLieferplanung = new LieferplanungModel({
