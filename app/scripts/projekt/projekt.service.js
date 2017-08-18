98db6eadf8e16ba1bda451bd59ea3f9976c553cd
Mikel Cordovilla <mikel.cordovilla@tegonal.com>
2017-08-18 12:34:23 +0200

    OO-556 adding the messages and the maintenance mode in the frontend

--- app/scripts/projekt/projekt.service.js
+++ app/scripts/projekt/projekt.service.js
@@ -1,21 +1,25 @@
 'use strict';
-
 /**
  */
 angular.module('openolitor-admin')
-  .factory('ProjektService', ['$rootScope', 'ProjektModel', 'msgBus', 'ooAuthService', '$q',
-    function($rootScope, ProjektModel, msgBus, ooAuthService, $q) {
+  .factory('ProjektService', ['$rootScope', 'OpenProjektModel','ProjektModel', 'msgBus', 'ooAuthService', '$q',
+    function($rootScope, OpenProjektModel, ProjektModel, msgBus, ooAuthService, $q) {
 
       var projekt;
 
-      var loadProjekt = function() {
+      var loadProjekt = function(openProjekt) {
         var deferred = $q.defer();
 
         ooAuthService.resolveUser().then(function() {
-          if (projekt) {
-            deferred.resolve(projekt);
-          } else {
-            ProjektModel.query({}, function(result) {
+            if (!openProjekt){
+                ProjektModel.query({}, function(result) {
+                    projekt = result;
+                    deferred.resolve(projekt);
+                }, function(error) {
+                    deferred.reject(error);
+                });
+          }else{
+             OpenProjektModel.query({}, function(result) {
               projekt = result;
               deferred.resolve(projekt);
             }, function(error) {
@@ -23,7 +27,6 @@ angular.module('openolitor-admin')
             });
           }
         });
-
         return deferred.promise;
       };
 
