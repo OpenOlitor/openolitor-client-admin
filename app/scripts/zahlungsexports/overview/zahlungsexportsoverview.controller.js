1cf9682ef0b27611a3f1fa40226d5f62de362ead
Mikel Cordovilla <mikel.cordovilla@tegonal.com>
2018-03-09 16:52:22 +0100

    OO-sunu #14 adding the zahlung export overview endpoints

---
+++ app/scripts/zahlungsexports/overview/zahlungsexportsoverview.controller.js
@@ -0,0 +1,93 @@
+'use strict';
+
+/**
+ */
+angular.module('openolitor-admin')
+  .controller('ZahlungsExportsOverviewController', ['$q', '$scope', '$filter', '$location',
+    'ZahlungsExportsOverviewModel', 'NgTableParams', 'localeSensitiveComparator',
+    function($q, $scope, $filter, $location, ZahlungsExportsOverviewModel, NgTableParams,
+      localeSensitiveComparator) {
+
+      $scope.entries = [];
+      $scope.loading = false;
+
+      $scope.search = {
+        query: ''
+      };
+
+      $scope.hasData = function() {
+        return $scope.entries !== undefined;
+      };
+
+      if (!$scope.tableParams) {
+        //use default tableParams
+        $scope.tableParams = new NgTableParams({ // jshint ignore:line
+          page: 1,
+          count: 10,
+          sorting: {
+            name: 'asc'
+          }
+        }, {
+          filterDelay: 0,
+          groupOptions: {
+            isExpanded: true
+          },
+          getData: function(params) {
+            if (!$scope.entries) {
+              return;
+            }
+            // use build-in angular filter
+            var dataSet = $filter('filter')($scope.entries, $scope.search.query);
+            // also filter by ngtable filters
+            dataSet = $filter('filter')(dataSet, params.filter());
+            dataSet = params.sorting ?
+              $filter('orderBy')(dataSet, params.orderBy(), true, localeSensitiveComparator) :
+              dataSet;
+
+            params.total(dataSet.length);
+            return dataSet.slice((params.page() - 1) * params.count(), params.page() * params.count());
+          }
+
+        });
+      }
+
+      $scope.prueba = function(rechnungen){
+          $location.path('/rechnungen').search('q', 'id=' + rechnungen).search('tf','{"status":""}')
+      }
+      $scope.download = function(filename, text){
+        var element = document.createElement('a');
+            element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+            element.setAttribute('download', filename);
+
+            element.style.display = 'none';
+            document.body.appendChild(element);
+
+            element.click();
+
+            document.body.removeChild(element);
+      }
+        
+      function search() {
+        if ($scope.loading) {
+          return;
+        }
+        //  $scope.entries = $scope.dummyEntries;
+        $scope.tableParams.reload();
+
+        $scope.loading = true;
+        $scope.entries = ZahlungsExportsOverviewModel.query({
+          q: $scope.query
+        }, function() {
+          $scope.tableParams.reload();
+          $scope.loading = false;
+        });
+      }
+
+      search();
+
+      $scope.$watch('search.query', function() {
+        search();
+      }, true);
+
+    }
+  ]);
