754baa445c0e4825bc6bb558f8cc111ca660b667
Alwin Egger <alwin.egger@tegonal.com>
2018-10-08 16:49:06 +0200

    Added Zusatzabos-Overview

--- app/scripts/abos/overview/abosoverviewcreaterechnungspositionen.controller.js
+++ app/scripts/abos/overview/abosoverviewcreaterechnungspositionen.controller.js
@@ -2,17 +2,52 @@
 
 /**
  */
-angular.module('openolitor-admin')
-  .controller('AbosOverviewCreateRechnungsPositionenController', ['$scope', '$filter', '$routeParams',
-    '$location', '$route', '$uibModal', '$log', '$http', 'gettext',
-    'moment', 'EnumUtil', 'DataUtil', 'msgBus', '$q', 'lodash',
-    'API_URL', 'alertService', 'AbosOverviewService','ooAuthService',
+angular
+  .module('openolitor-admin')
+  .controller('AbosOverviewCreateRechnungsPositionenController', [
+    '$scope',
+    '$filter',
+    '$routeParams',
+    '$location',
+    '$route',
+    '$uibModal',
+    '$log',
+    '$http',
+    'gettext',
+    'moment',
+    'EnumUtil',
+    'DataUtil',
+    'msgBus',
+    '$q',
+    'lodash',
+    'API_URL',
+    'alertService',
+    'AbosOverviewService',
+    'ZusatzabosOverviewService',
+    'ooAuthService',
 
-    function($scope, $filter, $routeParams, $location, $route, $uibModal,
-      $log, $http, gettext,
-      moment, EnumUtil, DataUtil, msgBus, $q, _, API_URL,
-      alertService, AbosOverviewService, ooAuthService) {
-
+    function(
+      $scope,
+      $filter,
+      $routeParams,
+      $location,
+      $route,
+      $uibModal,
+      $log,
+      $http,
+      gettext,
+      moment,
+      EnumUtil,
+      DataUtil,
+      msgBus,
+      $q,
+      _,
+      API_URL,
+      alertService,
+      AbosOverviewService,
+      ZusatzabosOverviewService,
+      ooAuthService
+    ) {
       $scope.rechnungsPositionen = {
         waehrung: 'CHF'
       };
@@ -36,26 +71,42 @@ angular.module('openolitor-admin')
       $scope.batchCreate = function() {
         $scope.user = ooAuthService.getUser();
         $scope.rechnungsPositionen.ids = $scope.aboIds;
-        switch($scope.form.mode) {
+        var service =
+          $scope.aboType === 'zusatzabos'
+            ? ZusatzabosOverviewService
+            : AbosOverviewService;
+        switch ($scope.form.mode) {
           case 'AnzahlLieferungen':
-            AbosOverviewService.createAnzahlLieferungenRechnungsPositionen($scope.rechnungsPositionen).then(function() {
-              $scope.commandIssued = true;
-              $scope.createHasWorked = true;
-            }, function (){
-              $scope.commandIssued = true;
-              $scope.createHasWorked = false;
-              console.log('AnzahlLieferungen error');
-            });
+            service
+              .createAnzahlLieferungenRechnungsPositionen(
+                $scope.rechnungsPositionen
+              )
+              .then(
+                function() {
+                  $scope.commandIssued = true;
+                  $scope.createHasWorked = true;
+                },
+                function() {
+                  $scope.commandIssued = true;
+                  $scope.createHasWorked = false;
+                  console.log('AnzahlLieferungen error');
+                }
+              );
             break;
           case 'BisGuthaben':
-             AbosOverviewService.createBisGuthabenRechnungsPositionen($scope.rechnungsPositionen).then(function() {
-              $scope.commandIssued = true;
-              $scope.createHasWorked = true;
-            }, function (){
-              $scope.commandIssued = true;
-              console.log('BisGuthaben error');
-              $scope.createHasWorked = false;
-            });
+            service
+              .createBisGuthabenRechnungsPositionen($scope.rechnungsPositionen)
+              .then(
+                function() {
+                  $scope.commandIssued = true;
+                  $scope.createHasWorked = true;
+                },
+                function() {
+                  $scope.commandIssued = true;
+                  console.log('BisGuthaben error');
+                  $scope.createHasWorked = false;
+                }
+              );
             break;
         }
       };
@@ -68,7 +119,9 @@ angular.module('openolitor-admin')
       };
 
       $scope.jumpToRechnungspositionen = function() {
-        $location.path('/rechnungspositionen').search({'q': 'id=' + $scope.batchCreated.ids.join()});
+        $location
+          .path('/rechnungspositionen')
+          .search({ q: 'id=' + $scope.batchCreated.ids.join() });
       };
 
       $scope.jumpToAbosWhereCreateHasFailed = function() {
@@ -77,7 +130,7 @@ angular.module('openolitor-admin')
 
       msgBus.onMsg('EntityCreated', $scope, function(event, msg) {
         if (msg.entity === 'RechnungsPosition') {
-          if($scope.user.id === msg.data.modifikator) {
+          if ($scope.user.id === msg.data.modifikator) {
             $scope.batchCreated.ids.push(msg.data.id);
             _.pull($scope.batchCreated.openAboIds, msg.data.aboId);
             $scope.$apply();
---
+++ app/scripts/abos/overview/zusatzabosoverview.service.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ */
+angular.module('openolitor-admin').factory('ZusatzabosOverviewService', [
+  '$http',
+  'API_URL',
+  function($http, API_URL) {
+    function createAnzahlLieferungenRechnungsPositionen(
+      rechnungsPositionCreate
+    ) {
+      return $http.post(
+        API_URL + 'abos/aktionen/anzahllieferungenrechnungspositionen',
+        rechnungsPositionCreate
+      );
+    }
+
+    function createBisGuthabenRechnungsPositionen(rechnungsPositionCreate) {
+      return $http.post(
+        API_URL + 'abos/aktionen/bisguthabenrechnungspositionen',
+        rechnungsPositionCreate
+      );
+    }
+
+    var service = {
+      createAnzahlLieferungenRechnungsPositionen: createAnzahlLieferungenRechnungsPositionen,
+      createBisGuthabenRechnungsPositionen: createBisGuthabenRechnungsPositionen
+    };
+
+    return service;
+  }
+]);
