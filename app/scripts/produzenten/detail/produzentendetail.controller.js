20bbdab4c812972df95ffbd8fd56a4d70ebdf1fa
Mikel Cordovilla <mikel.cordovilla@tegonal.com>
2020-04-09 12:41:15 +0200

    OO-check produzent nickname without taking into account accents

--- app/scripts/produzenten/detail/produzentendetail.controller.js
+++ app/scripts/produzenten/detail/produzentendetail.controller.js
@@ -55,11 +55,13 @@ angular.module('openolitor-admin')
       $scope.save = function() {
         var listOfNicknames = lodash.map($scope.produzenten, 'kurzzeichen');
         //new producer
-        if (isNew && lodash.filter(listOfNicknames, function(n){ return n === $scope.produzent.kurzzeichen}).length > 0){
+        //normalization in order to remove the accents. https://stackoverflow.com/questions/990904/remove-accents-diacritics-in-a-string-in-javascript/37511463#37511463
+        if (isNew && lodash.filter(listOfNicknames, function(n){ return n === $scope.produzent.kurzzeichen.normalize('NFD').replace(/[\u0300-\u036f]/g, "")}).length > 0){
             alertService.addAlert('error', gettext('Dieser Produzent existiert bereits'));
             return "";
         //existing producer
-        } else if (!isNew && originalNickname !== $scope.produzent.kurzzeichen && lodash.filter(listOfNicknames, function(n){ return n === $scope.produzent.kurzzeichen}).length > 0) { 
+        //normalization in order to remove the accents. https://stackoverflow.com/questions/990904/remove-accents-diacritics-in-a-string-in-javascript/37511463#37511463
+        } else if (!isNew && originalNickname !== $scope.produzent.kurzzeichen && lodash.filter(listOfNicknames, function(n){ return n === $scope.produzent.kurzzeichen.normalize('NFD').replace(/[\u0300-\u036f]/g, "")}).length > 0) { 
             alertService.addAlert('error', gettext('Dieser Produzent existiert bereits'));
             return "";
         } else {
