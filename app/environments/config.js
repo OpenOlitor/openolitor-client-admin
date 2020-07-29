c113cb9296a55eb02468b21d467a2249ce6c476c
Alwin Egger <alwin.egger@tegonal.com>
2020-07-29 08:01:09 +0200

    merge

--- app/environments/config.js
+++ app/environments/config.js
@@ -2,13 +2,8 @@
 
 function getConfig() {
   return {
-<<<<<<< Updated upstream
     "API_URL": "http://localhost:9003/try/",
     "API_WS_URL": "http://localhost:9003/try/ws",
-=======
-    "API_URL": "http://localhost:8080/m1/",
-    "API_WS_URL": "http://localhost:8080/m1/ws",
->>>>>>> Stashed changes
     "ENV": "dev",
     "version": "2.4.0",
     "AIRBREAK_API_KEY": "48f4d0be704fafd7ed7b4fdf2d2119d9",
