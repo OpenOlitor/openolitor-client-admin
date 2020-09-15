4b0507d3ebdff2701806563d92fc315b6540d1f7
Alwin Egger <alwin.egger@tegonal.com>
2020-09-15 21:00:46 +0200

    use core 2.5.2; included missing font-files

--- Gruntfile.js
+++ Gruntfile.js
@@ -241,7 +241,7 @@ module.exports = function(grunt) {
             '<%= openolitor.dist %>/scripts/**/*.js',
             '<%= openolitor.dist %>/styles/**/*.css',
             '<%= openolitor.dist %>/images/**/*.{png,jpg,jpeg,gif,webp,svg}',
-            '<%= openolitor.dist %>/styles/fonts/*',
+            '<%= openolitor.dist %>/styles/fonts/**/*.woff2',
           ]
         }
       }
@@ -360,7 +360,7 @@ module.exports = function(grunt) {
               'scripts/**/*.html',
               'images/**/*.{webp,png,jpg}',
               'third_party/**',
-              'fonts/*'
+              'fonts/**/*.woff2'
             ]
           },
           {
