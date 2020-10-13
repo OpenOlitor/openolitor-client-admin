f1a6ae305e8da441e10e5c036d270d023db1128a
Alwin Egger <alwin.egger@tegonal.com>
2020-10-13 15:01:42 +0200

    load config.js for tests

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -49,6 +49,7 @@ module.exports = function(config) {
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js',
+      'app/environments/config.js',
       'app/scripts/**/*.html'
     ],
 
