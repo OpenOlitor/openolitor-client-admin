61d12ab107c39ae7c4eafcb513296a5895fc013b
Mikel Cordovilla <mikel.cordovilla@tegonal.com>
2022-03-30 15:49:34 +0200

    OO#410 secondary links on another window and with icon

--- app/scripts/abotypen/detail/abotypendetail.controller.js
+++ app/scripts/abotypen/detail/abotypendetail.controller.js
@@ -3,11 +3,11 @@
 /**
  */
 angular.module('openolitor-admin')
-  .controller('AbotypenDetailController', ['$scope', '$filter', '$routeParams',
+  .controller('AbotypenDetailController', ['$scope', '$rootScope', '$filter', '$routeParams',
     '$location', 'gettext', 'NgTableParams', 'AbotypenDetailModel','ZusatzAbotypenDetailModel', 'msgBus',
     'LIEFERRHYTHMEN', 'PREISEINHEITEN', 'LAUFZEITEINHEITEN', 'FRISTEINHEITEN',
     'EnumUtil',
-    function($scope, $filter, $routeParams, $location, gettext, NgTableParams,
+    function($scope, $rootScope, $filter, $routeParams, $location, gettext, NgTableParams,
       AbotypenDetailModel, ZusatzAbotypenDetailModel, msgBus, LIEFERRHYTHMEN, PREISEINHEITEN,
       LAUFZEITEINHEITEN, FRISTEINHEITEN,
       EnumUtil) {
