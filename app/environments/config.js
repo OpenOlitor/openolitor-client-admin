6ffee84db50b3cb1a9e6b3eabe38099a061a3317
Mikel Cordovilla <mikel.cordovilla@tegonal.com>
2023-02-06 11:35:20 +0100

    bumped version to 2.6.10

--- app/environments/config.js
+++ app/environments/config.js
@@ -2,8 +2,8 @@
 
 function getConfig() {
   return {
-    "API_URL": "http://localhost:9001/api-try/",
-    "API_WS_URL": "http://localhost:9001/api-try/ws",
+    "API_URL": "http://localhost:9001/api-csa1/",
+    "API_WS_URL": "http://localhost:9001/api-csa1/ws",
     "ENV": "dev",
     "version": "2.6.0",
     "EMAIL_TO_ADDRESS": "info@tegonal.com",
