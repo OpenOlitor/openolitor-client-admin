e257748dfa8452ed70c45436d71b7aecd5506bb5
Mikel Cordovilla <mikel.cordovilla@tegonal.com>
2023-05-31 16:56:49 +0200

    OO/abos_code_can_have_more_than_5_digits

--- app/scripts/pendenzen/overview/pendenzen.service.js
+++ app/scripts/pendenzen/overview/pendenzen.service.js
@@ -5,14 +5,14 @@
 angular.module('openolitor-admin')
   .factory('PendenzenService', ['$rootScope', 
     function($rootScope) {
-      var regexAbo = /Abo Nr.: \d\d\d\d\d|Abo Nr.:\d\d\d\d\d/g;
-      var regexCode = /\d\d\d\d\d/g;
+      var regexAbo = /Abo Nr.: \d+|Abo Nr.:\d+/g;
+      var regexCode = /\d+/g;
 
       return {renderText(text ) {
         if (text !== undefined) {
           var foundValue = text.match(regexAbo);
           if (foundValue) {
-            var code = text.match(regexCode);
+            var code = foundValue[0].match(regexCode);
             var value = text.replace(foundValue, foundValue + ' <a target="_blank" rel="noopener noreferrer" href="#/abos?q=id%3D' + code + '"><i class="glyphicon small glyphicon-new-window"></i></a></b>')
             return value;
           } else {
